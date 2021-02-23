afeb0cd91b6031325103597c438cdd8a2d668542
Betzabe Silva <betzabesp94@gmail.com>
2021-02-22 21:56:23 -0500

    kivymd

--- hello_kivy_world.py
+++
@@ -1,8 +0,0 @@
-from kivy.app import App
-from kivy.uix.button import Button
-
-class hello_kivy_worldApp(App):
-    def build(self):
-        return Button(text='Hello World')
-
-hello_kivy_worldApp().run()
